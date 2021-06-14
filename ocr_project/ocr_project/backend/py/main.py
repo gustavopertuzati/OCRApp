890971774859f712195a04d79de120f7ecf0dc96
gustavo.pertuzat <gustavo.pertuzat@hes-so.ch>
2021-06-14 18:01:48 +0200

    type check ok with mypy

--- ocr_project/ocr_project/backend/py/main.py
+++ ocr_project/ocr_project/backend/py/main.py
@@ -3,21 +3,25 @@ from pydantic import BaseModel
 from fastapi import FastAPI, Query
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.responses import JSONResponse
-from model import trainModel, guessModel 
+from model import trainModel, guessModel
 
 app = FastAPI()
 
+
 class Item(BaseModel):
     content: List[int]
+
 
 class ItemValue(BaseModel):
     value: int
     tab: List[int]
 
+
 @app.post("/guess/")
 async def guess_value(item: Item):
-    content=guessModel(item.content).tolist()
+    content = guessModel(item.content).tolist()
     return JSONResponse(content)
+
 
 @app.post("/train/")
 async def train_Ai(item: ItemValue):
--- ocr_project/ocr_project/backend/py/model.py
+++ ocr_project/ocr_project/backend/py/model.py
@@ -1,38 +1,41 @@
 import tensorflow as tf
-import  numpy as np
-from tensorflow.python.keras.backend import zeros    
+import numpy as np
+from tensorflow.python.keras.backend import zeros
+
 
 def createModel():
-    
+
     model = tf.keras.Sequential()
-    model.add(tf.keras.layers.Input(shape=(20*20)))
+    model.add(tf.keras.layers.Input(shape=(20 * 20)))
     model.add(tf.keras.layers.Dense(128, activation=tf.nn.relu))
     model.add(tf.keras.layers.Dense(10, activation=tf.nn.softmax))
-    model.compile(loss= "mse", optimizer='Adam')
+    model.compile(loss="mse", optimizer="Adam")
     model.summary()
     return model
+
 
 def trainModel(train_features, label_features):
 
     try:
-        model = tf.keras.Model.load_model('ocr_model.h5')
+        model = tf.keras.Model.load_model("ocr_model.h5")
     except:
         model = createModel()
 
-    label = np.zeros(10).reshape((1,10))
+    label = np.zeros(10).reshape((1, 10))
     label[0][label_features] = 1
     train = np.reshape(train_features, (1, 400))
     model.fit(train, label, batch_size=1, epochs=2)
-    model.save('ocr_model.h5')
+    model.save("ocr_model.h5")
+
 
 def guessModel(guess_features):
 
     try:
-        model = tf.keras.Model.load_model('ocr_model.h5')
+        model = tf.keras.Model.load_model("ocr_model.h5")
     except:
         model = createModel()
 
     guess = np.reshape(guess_features, (1, 400))
     predict = model.predict(guess)[0, :]
-    model.save('ocr_model.h5')
+    model.save("ocr_model.h5")
     return predict
